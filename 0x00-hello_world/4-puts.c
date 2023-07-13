6a45fe8a4ee818b488ae3c7652e8ac9f4c6cc98d
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-13 16:29:39 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: program print one line
+ *
+ * return: 0 (sucess)
+ */
+int main (void)
+{
+puts ("/"Programming is like building a multilingual puzzle\n");
+return (0);
+}
