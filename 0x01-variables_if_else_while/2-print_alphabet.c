79c12f841915f73a0c8ee1062f1a1f09f175998e
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-14 15:08:47 -0700

    sss

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A c program that prints the alphabet with putchar function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+return (0);
+}
