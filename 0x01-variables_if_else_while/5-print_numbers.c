a86212d2a62233512d5f88253c4869e4d219baf9
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-14 16:00:49 -0700

    easy

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A c program that prints digit numbers with printf function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d\n", a);
+return (0);
+}
