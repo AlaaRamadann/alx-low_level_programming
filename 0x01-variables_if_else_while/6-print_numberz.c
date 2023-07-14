77a10357fbd75ba79685c90a3bc510f969a2bd3d
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-14 16:10:04 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A c program that prints digit numbers with putchar function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int a;
+for (a = '0'; a < '10'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
