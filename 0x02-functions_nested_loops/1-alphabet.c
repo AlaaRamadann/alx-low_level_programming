65b239f3e04bbe0398f957465f96704879845aba
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-18 18:03:15 -0700

    abc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: A c program that prints with puts function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+return (0);
+}
