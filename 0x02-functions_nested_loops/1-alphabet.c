2b60a5273b3e6aaea2ded24e78cb160a2f388e6d
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-18 18:30:47 -0700

    ?

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-/**
-* print_alphabet - utilizes _putchar function to print alphabet
-*
-*/
 void print_alphabet(void)
 {
 int c;
 for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
+}
+/**
+* main - Entry point
+*
+* print_alphabet -  print alphabet utilizes _putchar function
+*
+*  Return: Always 0 (success)
+*/
+int main(void)
+{
+print_alphabet();
 return (0);
 }
