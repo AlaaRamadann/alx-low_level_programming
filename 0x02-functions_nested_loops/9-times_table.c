e1c80803b64aa73989e0b2cdd499c756a081ee3e
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-22 20:27:00 -0700

    alaa

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,16 @@ for (m = 0; m <= 9; m++)
 {
 if ((g * m)  <= 9)
 {
-_putchar(' ');	
-_putchar((g * m) + 48);
-_putchar(',');
 _putchar(' ');
+_putchar((g * m) + 48);
 }
 else
 {
 _putchar(((g * m) / 10) + 48);
 _putchar(((g * m) % 10) + 48);
+}
+if (m != 9)
+{
 _putchar(',');
 _putchar(' ');
 }
