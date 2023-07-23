0eca16fd17c7643ba4be1e6f3aa29c79f71b57cb
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-22 21:04:07 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - function that adds two integers
+ *@x: check for number
+ *
+ *@y: check for number
+ *
+ *Return: returns the result
+ */
+int add(int x, int y)
+{
+int a = x + y;
+_putchar(a);
+return (a);
+}
