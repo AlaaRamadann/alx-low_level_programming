70f29c837e30b17785fb0a492ee467a8af12cee2
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-23 17:54:39 -0700

    aa

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  more_numbers - function that prints 10 times the numbers
+ *
+ *  Description: A c program prints from 0 to 14
+ *
+ *  Return: no return
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 13; j++)
+{
+if (j > 9)
+{
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
+}
+else
+_putchar(j + 48);
+}
+_putchar('\n');
+}
+}
