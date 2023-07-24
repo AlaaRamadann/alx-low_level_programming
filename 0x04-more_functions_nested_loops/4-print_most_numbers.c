6a066078e57b3e44d123cb52121424be7f4995cf
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-23 17:43:29 -0700

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_most_numbers - function that prints the numbers, from 0 to 9
+ *
+ *  Description: A c program prints digits
+ *
+ *  Return: no return
+ */
+void print_most_numbers(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+if (x != 50 || x != 52)
+_putchar(x);
+}
+_putchar('\n');
+}
