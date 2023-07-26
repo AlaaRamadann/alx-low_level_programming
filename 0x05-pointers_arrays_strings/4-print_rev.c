3330b2d8d7f1ac2aa5e9727973d9cccb9bcb6155
AlaaRamadann <alaaaaramadan@gmailcom>
2023-07-26 03:05:20 -0700

    reverse by length

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string in reverse
+ *
+ * @s: string want to reverse
+ */
+void print_rev(char *s)
+{
+int length, i;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
