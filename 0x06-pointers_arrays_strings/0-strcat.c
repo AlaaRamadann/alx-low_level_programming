d6bcaf88bd82e33e5f88bcf6c3f2969da7eb9f6c
AlaaRamadann <alaaaaramadan@gmailcom>
2023-08-12 13:34:18 -0700

    for loop

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,9 @@ char *_strcat(char *dest, char *src)
 int i, j;
 j = 0;
 for (i = 0; dest[i] != '\0'; i++)
-for (; src[j] != '\0'; i++)
+for (; src[j] != '\0'; i++, j++)
 {
 dest[i] = src[j];
-j++;
 }
 dest[i] = '\0';
 return (dest);
