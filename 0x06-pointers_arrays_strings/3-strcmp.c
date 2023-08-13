a1a40fbdb24cb315ed63c9fb77cc818a76cd78e2
AlaaRamadann <alaaaaramadan@gmailcom>
2023-08-13 04:42:45 -0700

    loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,25 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 i = 0;
-while ()
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s[i] == '\0' && s1[i] == s2[i])
-{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+i++;
+}
 return (0);
-break;
-}
-if (s1[i] == s2[i])
-i++;
-if (s1[i] > s2[i])
-{
-return (s1[i] - s2[i]);
-break;
-}
-else if (s1[i] < s2[i])
-{
-return (s1[i] - s2[i]);
-break;
-}
-i++;
-}
 }
