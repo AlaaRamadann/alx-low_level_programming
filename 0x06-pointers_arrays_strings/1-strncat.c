fd1baa7d9e45967487fc69fed30bceeaa9b098eb
AlaaRamadann <alaaaaramadan@gmailcom>
2023-08-13 02:23:20 -0700

    l

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,9 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j, l;
-l = 0;
-for (i = 0; src[i] != '\0'; i++)
-{
-l++;
-}
+int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 {
-}
-if (n >= l)
-{
-for (j = 0; src[j] != '\0'; i++, j++)
-{
-dest[i] = src[j];
-}
 }
 for (j = 0; j <= n - 1 && src[j] != '\0'; i++, j++)
 {
